29fdee71dec6e51d01dc99ea2b13a34c3b6f946d
Ajay Sonere <ajaysonere786@gmail.com>
2022-10-30 17:28:23 +0300

    Rotate Array

--- Complete DSA In C++/1. Array/rotatearraybyk.cpp
+++ Complete DSA In C++/1. Array/rotatearraybyk.cpp
@@ -1,23 +1,50 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 void rotateArrayByK(int arr[],int size,int k){
     int ans[size]={0};
     int index =0;
-    for(int i=k;i<size;i++){
+    for(int i=k+1;i<size;i++){
         ans[index]=arr[i];
         index++;
     }
-    for(int i=0;i<k;i++){
+    for(int i=0;i<=k;i++){
       ans[index] = arr[i];
       index++;
     }
+    cout << "Return By Array :" ;
     for(int i=0;i<size;i++){
         cout << ans[i] << " ";
     }
     cout << "\n";
 }
+void reverse(vector<int> &a,int start,int end){
+    while(start<end){
+       int temp = a[start];
+       a[start] = a[end];
+       a[end] = temp;
+       start++;
+       end--;
+    }
+}
+vector<int>  rotateVectorKtimes(vector<int> a,int k){
+    int n = a.size();
+    k = k%n;
+
+    if(k<0){
+        k += n;
+    }
+    reverse(a,0,n-1);
+    reverse(a,0,k-1);
+    reverse(a,k,n-1);
+    return a;
+}
+
+
+
 int main ()
-{int size;
+{
+    int size;
 cin >> size;
  int arr[size];
  for(int i=0;i<size;i++){
@@ -26,5 +53,18 @@ cin >> size;
  int k;
  cin>> k;
  rotateArrayByK(arr,size,k);
+
+// Time complexity will be O(n)
+ vector<int> v;
+ for(int i=0;i<size;i++){
+    v.push_back(arr[i]);
+ }
+ vector<int> ans =  rotateVectorKtimes(v,k);
+ cout << "Return By Vector :";
+ for(int i=0;i<size;i++){
+    cout << ans[i] << " ";
+ }
+cout << "\n";
+
  return 0;
 }
