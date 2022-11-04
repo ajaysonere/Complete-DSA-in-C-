7156ff2ec0c880423aeccf790fc492621cd5b93c
Ajay Sonere <ajaysonere786@gmail.com>
2022-11-05 00:10:46 +0530

    Insertion Sort

--- Complete DSA In C++/2. Searching and Sorting/insertionSort.cpp
+++ Complete DSA In C++/2. Searching and Sorting/insertionSort.cpp
@@ -1,9 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main ()
+// Insertion Sort Time completixy is O(n^2).
+
+void insertionSort(int arr[], int size)
 {
- int size;
- cout << size;
- return 0;
+    cout << "Function is called :" <<size <<"\n";
+    for (int i = 1; i < size; i++)
+    {
+        int current = arr[i]; // storing the sorted part of the array
+        int prev = i - 1;     // iterate throgh the unsorted array
+
+        // For put the element at the right position
+        while (i >= 0 and arr[prev] > current)
+        {
+            arr[prev + 1] = arr[prev];
+            prev = prev - 1;
+            cout << "Inside loop :" << "\n";
+        }
+        arr[prev + 1] = current;
+        cout << "1" << " ";
+    }
+    cout << "Insertion Sort :" << "\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+     cout << "Insertion Sort :" << "\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    insertionSort(arr, size);
+     
+    cout << "\n";
+    return 0;
 }
