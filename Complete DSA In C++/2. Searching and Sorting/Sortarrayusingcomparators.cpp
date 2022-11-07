207b8d4bd6a72bddea79966cd3b4de5dc0801127
Ajay Sonere <ajaysonere786@gmail.com>
2022-11-07 22:34:35 +0530

    commit

--- Complete DSA In C++/2. Searching and Sorting/Sortarrayusingcomparators.cpp
+++ Complete DSA In C++/2. Searching and Sorting/Sortarrayusingcomparators.cpp
@@ -1,8 +1,47 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+
+void sortWithComparators(vector<int> &vec, bool flag){
+    if(flag ==1){
+        for(int pos = 1; pos<vec.size();pos++){
+            int current = vec[pos];
+            int prev = pos-1;
+            while(prev >=0 && vec[prev]>current){
+                vec[prev+1] = vec[prev];
+                prev--;
+            }
+            vec[prev+1] = current;
+        }
+    }
+    else{
+        reverse(vec.begin(),vec.end());
+    }
+}
 
 int main ()
 {
-
-return 0;
+ 
+ int size;
+ cin >> size;
+ vector<int> vec;
+ int value;
+ for(int i=0;i<size;i++){
+    cin >> value;
+    vec.push_back(value);
+ }
+ cout << " Before the sort : "<< "\n"; 
+ for(int x:vec){
+   cout << x << " ";
+ }
+ cout << "Enter the True or flase value " << "\n";
+ bool flag;
+ cin >> flag;
+ sortWithComparators(vec,flag);
+ cout << "After the sort :" << "\n";
+ for(int x: vec){
+    cout << x << " ";
+ }
+ return 0;
 }
