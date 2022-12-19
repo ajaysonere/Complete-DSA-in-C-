e56fdf5a3b29105118021cd055f5352a68b277d8
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-19 14:47:46 +0530

    CountDigit

--- Complete DSA In C++/03. Character array and String/countDigitandNumber.cpp
+++ Complete DSA In C++/03. Character array and String/countDigitandNumber.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 int main ()
 {
+char ch;
 
+ch = cin.get();
+
+int alpha =0;
+int space =0;
+int digit =0;
+
+while(ch != '\n'){
+    if(ch>='0' and ch <='9'){
+        digit++;
+    }
+    else if((ch >= 'a' and ch <='z') or (ch >='A' and ch <= 'Z')){
+        alpha++;
+    }else if(ch == ' ' or ch == '\t'){
+        space++;
+    }
+    ch = cin.get();
+}
+
+cout << "Total Alphabets :" << alpha << "\n";
+cout << "Total Digits :" << digit << "\n";
+cout << "Total Space :" << space << "\n";
 return 0;
 }
