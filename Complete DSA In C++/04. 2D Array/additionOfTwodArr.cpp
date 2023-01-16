0c2baf630d34a1b9f617103da9fa5aca305979ab
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-16 19:20:25 +0530

    Matrix

---
+++ Complete DSA In C++/04. 2D Array/additionOfTwodArr.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "Enter the Number of Rows And Column " << "\n";
+    int row,column;
+    cin >> row >> column;
+    // Declaring a Two D Array
+    
+    cout << "Enter the Element of the Arr 1 \n";
+    int arr1[row][column];
+    // Taking input in Two D array 
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cin >> arr1[i][j];
+        }
+    }
+
+    cout << "Enter the Element of the Arr 2 \n";
+    int arr2[row][column];
+    // Taking input in Two D array
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            cin >> arr2[i][j];
+        }
+    }
+
+    int sum[row][column];
+
+
+    // Geting 2 D  array 
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            sum[i][j] = arr1[i][j]+arr2[i][j];
+        }
+        cout << "\n";
+    }
+
+    cout << "Sum of the Two Arrays : \n";
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cout << sum[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
--- Complete DSA In C++/04. 2D Array/basic.c++
+++ Complete DSA In C++/04. 2D Array/basic.c++
@@ -3,6 +3,26 @@ using namespace std;
 
 int main ()
 {
- cout << "2D Array";
-return 0;
+//  cout << "2D Array";
+   cout << "Enter the Number of the Row And Column \n";
+   int row , column;
+   cin >> row >> column;
+   // Declaring a Two D array
+   int arr[row][column];
+   // Taking input in Two D array    
+   for(int i=0;i<row;i++){
+      for(int j=0;j<column;j++){
+         cin >> arr[i][j];
+      }
+   }
+
+   // Printing the Two D array  
+   for(int i=0;i<row;i++){
+       for(int j=0;j<column;j++){
+            cout << arr[i][j] << " ";
+        }
+       cout << "\n";
+   }
+
+ return 0;
 }
