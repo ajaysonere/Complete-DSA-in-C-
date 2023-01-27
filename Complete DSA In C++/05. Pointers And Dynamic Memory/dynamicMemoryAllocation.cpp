1d07e79395e4dc63cfb2295813c417ab76cb5cd6
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-27 22:26:25 +0530

    Dynamic array

--- Complete DSA In C++/05. Pointers And Dynamic Memory/dynamicMemoryAllocation.cpp
+++ Complete DSA In C++/05. Pointers And Dynamic Memory/dynamicMemoryAllocation.cpp
@@ -3,6 +3,33 @@ using namespace std;
 
 int main ()
 {
+    int size;
+    cin >> size;
+
+    // dynamic array 
+    int *arr = new int [size];
+    cout << arr << "\n";
+
+    // taking input 
+    for(int i=0;i<size;i++){
+         cin >> arr[i]; 
+    }
+
+    // print the array
+    for(int i=0;i<size;i++){
+        cout << arr[i] << " ";
+    }
+    cout <<"\n";
     
+    for(int i=0;i<size;i++){
+        cout << *(arr+i) << " ";
+    }
+    cout << "\n";
+
+    delete [] arr;
+    cout << arr << "\n";
+
+
+
     return 0;
 }
