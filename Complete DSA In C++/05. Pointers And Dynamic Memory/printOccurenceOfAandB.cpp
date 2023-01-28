d064fdbae390e67479d741c6d5ed42ad0cf4e579
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-28 13:04:18 +0530

    addition

--- Complete DSA In C++/05. Pointers And Dynamic Memory/printOccurenceOfAandB.cpp
+++ Complete DSA In C++/05. Pointers And Dynamic Memory/printOccurenceOfAandB.cpp
@@ -1,16 +1,39 @@
 #include<iostream>
 using namespace std ;
 
+void printFirstAndLastOccurecneOfA(string str, int *first, int *last)
+{
+
+    // for first occurence of the a 
+    for(int i=0; i<str.size(); i++){
+        if(str[i] == 'a'){
+            *first  = i;
+            break;
+        }
+    }
+
+    // for the last occurence of the a
+
+    for(int i= str.size()-1; i>=0 ; i--){
+        if(str[i] == 'a'){
+            *last  = i;
+            break;
+        }
+    }
+}
+
 int main(){
 
     string str;
     cin >> str ;
 
-    int *first {};
-    int *last {};
+    int first = -1;
+    int last = -1;
 
-    printFirstAndLastOccurecneOfA(str,first, last);
-    retrun 0;
+    printFirstAndLastOccurecneOfA(str, &first, &last);
+    cout << "First Occurence of A " << first << " Last Occurence of B " << last << "\n";
+
+    return 0;
 }
 
 
