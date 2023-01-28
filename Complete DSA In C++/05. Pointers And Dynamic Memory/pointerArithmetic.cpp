97f28b4bde9c4dfd0f7fa5a7b6908543a8aaf09e
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-28 14:10:07 +0530

    pointer

--- Complete DSA In C++/05. Pointers And Dynamic Memory/pointerArithmetic.cpp
+++ Complete DSA In C++/05. Pointers And Dynamic Memory/pointerArithmetic.cpp
@@ -3,10 +3,28 @@ using namespace std;
 
 int main ()
 {
-    int x  =10;
-    int *ptr = &x;
-    cout << ptr <<"\n";
-    ptr +=1;
-    cout << ptr <<"\n";
+    // int x  =10;
+    // int *ptr = &x;
+    // cout << ptr <<"\n";
+    // ptr +=1;
+    // cout << ptr <<"\n";
+
+    int arr[] = {1,2,3};
+    int *firstPtr = &arr[0];
+
+    // cout << *firstPtr << "  " << &arr[0] << "\n";
+    cout << firstPtr << "  " << *firstPtr << "\n";
+
+    // *firstPtr++;
+
+    // cout << *firstPtr++ << " " << *firstPtr++ << "\n";
+    cout << ++*firstPtr << '\n';
+
+    cout << *++firstPtr << '\n';
+
+
+    for(int i=0;i<3;i++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
