bad16850375fa678a32c5638ce0af8f1083360cb
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-29 14:35:08 +0530

    lowerandupper bound

--- Complete DSA In C++/06. Vector Data Structure/lowerAndUpper.cpp
+++ Complete DSA In C++/06. Vector Data Structure/lowerAndUpper.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main ()
 {
     cout << "FIND THE LOWER AND UPPER BOUND " << "\n";
+    int size;
+    cin >> size;
+    vector<int> vec(size,0);
+    for(int i=0;i<size;i++){
+        cin >> vec[i];
+    }
+    
+    sort(vec.begin(),vec.end());
+
+    int key;
+    cin >> key;
+    vector<int>::iterator low,up;
+    low  = lower_bound(vec.begin(),vec.end(),key);
+    up  = upper_bound(vec.begin(),vec.end(),key);
+     cout << *low  << " " << *up << "\n";
+    cout << "Lower Bound is :" << (low-vec.begin()) << "\n";
+    cout << "Upper Bound is :" << (up-vec.begin()) << "\n";
+
+
+
     return 0;
 }
