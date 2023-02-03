d20495a989acc85cf00f9b604228c198e5e39f68
Ajay Sonere <ajaysonere786@gmail.com>
2023-02-03 21:48:39 +0530

    conversion

--- Complete DSA In C++/07. Bit Manipulation/printBinary.cpp
+++ Complete DSA In C++/07. Bit Manipulation/printBinary.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
 using namespace std;
 
 // void printBinary(int n){
@@ -8,37 +10,48 @@ using namespace std;
 //     }
 // }
 
-int printBinary(int n){
-    int ans = 0;
-    int p=1;
+// int printBinary(int n){
+//     int ans = 0;
+//     int p=1;
+//     while(n>0){
+//         int lastBit = n&1;
+//         ans = ans+p*lastBit;
+//         p = p*10;
+//         n = n>>1;
+//     }
+//     return ans;
+// }
+
+int printDecimal(int n){
+    int ans =0;
+    int a = 1;
+    // while(n!=0){
+    //    int tmp = n%10;
+    //    n = n/10;
+    //    ans = ans+tmp*pow(2,a);
+    //    a++;
+    // }
+    // return ans;
+    
     while(n>0){
-        int lastBit = n&1;
-        ans = ans+p*lastBit;
-        p = p*10;
-        n = n>>1;
+        int lastbit = n&1;
+        n = n/10;
+        if(lastbit)
+            ans = ans+lastbit*a;
+        a = a<<1;
     }
     return ans;
-}
-
-int printDecimal(int m){
-    int ans = 0;
-    while(m>0){
-        int lastBit = m&1;
-        if(lastBit){
-            ans = ans*a;
-        }
-    }
 }
 
 int main ()
 {
 
-int n;
-cin >> n;
+// int n;
+// cin >> n;
 
-cout << printBinary(n) << "\n";
+// cout << printBinary(n) << "\n";
 int m;
 cin >> m;
-cout <<  printDecimal(m) << "\n";
+cout << printDecimal(m) << "\n";
 return 0;
 }
