ad8d0026d6dbafab156ac950c373b9e24604ccda
Ajay Sonere <ajaysonere786@gmail.com>
2023-02-17 22:24:35 +0530

    revesed linked list

--- Complete DSA In C++/13. Linked List/reverseLinkedList.cpp
+++ Complete DSA In C++/13. Linked List/reverseLinkedList.cpp
@@ -7,10 +7,65 @@ class Node{
     int data;
     Node* next;
 
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
 };
+
+void insertAtHead(Node* &head, int data){
+    if(head == NULL){
+        Node* temp = new Node(data);
+        head = temp;
+    }else{
+       Node* temp = new Node(data);
+       temp->next = head;
+       head = temp;
+    }
+}
+
+// reverse the linked list
+void reverseLinkedList(Node* &head){
+    Node* curr = head;
+    Node* prev = NULL;
+    Node* temp;
+    while(curr != NULL){
+        temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    head = prev;
+}
+
+void print(Node* head){
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
 
 int main ()
 {
+    Node* head = NULL;
 
+    insertAtHead(head,10);
+    // print(head);
+
+    insertAtHead(head,20);
+    // print(head);
+
+    insertAtHead(head,30);
+    // print(head);
+
+    insertAtHead(head,40);
+    // print(head);
+
+    insertAtHead(head,50);
+    print(head);
+    reverseLinkedList(head);
+    print(head);
     return 0;
 }
