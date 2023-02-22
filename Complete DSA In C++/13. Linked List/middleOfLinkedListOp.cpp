58ce4c5def5cdaad26d9ca7e7a57979864cd8273
Ajay Sonere <ajaysonere786@gmail.com>
2023-02-22 18:16:44 +0530

    slow and fast pointer

--- Complete DSA In C++/13. Linked List/middleOfLinkedListOp.cpp
+++ Complete DSA In C++/13. Linked List/middleOfLinkedListOp.cpp
@@ -1,8 +1,66 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main ()
+class Node
 {
-    
+
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int data)
+{
+    if (head == NULL)
+    {
+        Node *temp = new Node(data);
+        head = temp;
+    }
+    else
+    {
+        Node *temp = new Node(data);
+        temp->next = head;
+        head = temp;
+    }
+}
+
+Node *middleOfLinkedList(Node *head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+void print(Node *head){
+    while (head){
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    Node *head = NULL;
+    insertAtHead(head, 10);
+    insertAtHead(head, 20);
+    insertAtHead(head, 30);
+    insertAtHead(head, 40);
+    insertAtHead(head, 50);
+    insertAtHead(head, 60);
+    print(head);
+
+    cout << "midElement" << " ";
+    Node *temp = middleOfLinkedList(head);
+    cout << temp->data << "\n";
     return 0;
 }
