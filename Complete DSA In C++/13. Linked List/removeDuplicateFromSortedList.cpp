b6295f69a15908593091f685e4589dafb00d88b4
Ajay Sonere <ajaysonere786@gmail.com>
2023-02-26 10:30:20 +0530

    remove duplicate from sorted list

--- Complete DSA In C++/13. Linked List/removeDuplicateFromSortedList.cpp
+++ Complete DSA In C++/13. Linked List/removeDuplicateFromSortedList.cpp
@@ -1,7 +1,73 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main ()
+class Node
 {
-return 0;
+
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtTail(Node *&head, Node* &tail, int data)
+{
+    if (tail == NULL)
+    {
+        Node *temp = new Node(data);
+        tail = temp;
+        head = temp;
+    }
+    else
+    {
+        Node *temp = new Node(data);
+        tail->next = temp;
+        tail = temp;
+    }
+}
+
+void removeDuplicateFromList(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        if((temp->next != NULL) && (temp->data == temp->next->data)){
+            temp->next = temp->next->next;
+        }else{
+          temp = temp->next;
+       }
+    }
+}
+
+
+void print(Node *head)
+{
+    while (head)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node* tail = NULL;
+    insertAtTail(head, tail , 10);
+    insertAtTail(head, tail , 20);
+    insertAtTail(head, tail , 30);
+    insertAtTail(head, tail , 30);
+    insertAtTail(head, tail , 40);
+    insertAtTail(head, tail , 40);
+    insertAtTail(head, tail , 40);
+    print(head);
+    removeDuplicateFromList(head);
+    print(head);
+    
+
+    return 0;
 }
