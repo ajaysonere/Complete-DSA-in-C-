c7bc84efbd4a1b22e7e4c9b518ac821638245e42
Ajay Sonere <ajaysonere786@gmail.com>
2023-02-28 23:48:07 +0530

    stack

--- Complete DSA In C++/14. Stack/Stack.h
+++ Complete DSA In C++/14. Stack/Stack.h
@@ -1,1 +1,26 @@
 template <typename T>
+class Node{
+    public:
+    T data;
+    Node<T> *next;
+
+    Node(T data){
+        this->data = data;
+        this->next = 0;
+    }
+};
+
+template <typename T>
+class Stack{
+    Node<T> *head;
+
+    public:
+
+    Stack(){
+        head = 0;
+    }
+
+    void push(T data){
+        Node* temp = new <T>Node(data);
+    }
+};
