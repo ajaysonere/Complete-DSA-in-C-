44d9c38f4c20e9477050213c1f396e82fca84168
Ajay Sonere <ajaysonere786@gmail.com>
2023-02-28 23:17:59 +0530

    stack using list

--- Complete DSA In C++/14. Stack/stackImpUsingLinkedList.cpp
+++ Complete DSA In C++/14. Stack/stackImpUsingLinkedList.cpp
@@ -1,7 +1,81 @@
 #include<iostream>
 using namespace std;
 
+class Node{
+    
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+
+    ~Node(){
+        if(next == NULL){
+            delete next;
+            next = NULL;
+        }
+        cout << "data is deleted " << data << "\n";
+    }
+};
+
+class Stack{
+
+    public:
+    
+    Node* top ;
+     
+    Stack(){
+        top = NULL;
+    }
+
+    void push(int data){
+        Node* temp = new Node(data);
+        temp->next = top;
+        top = temp;
+    }
+
+    void pop(){
+        if(top == NULL){
+            cout << "Stack is Empty " << "\n";
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    int peek(){
+        if(top == NULL){
+            return -1;
+        }else{
+            return top->data;
+        }
+    }
+
+    bool isEmpty(){
+        return top == NULL;
+    }
+};
+
 int main ()
 { 
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    // cout << st.top->data << "\n";
+    st.pop();
+    st.pop();
+    st.pop();
+    cout << st.peek() << "\n";
+    st.push(50);
+    if(st.isEmpty()){
+        cout << "stack is empty \n";
+    }else{
+        cout << "stack is not empty \n";
+    }
+    cout << st.peek() << "\n";
     return 0;
 }
