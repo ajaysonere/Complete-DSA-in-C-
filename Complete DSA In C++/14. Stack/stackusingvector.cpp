740c29e9790716aff12b07a182ccca457a99932b
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-01 19:46:25 +0530

    stack using vector

--- Complete DSA In C++/14. Stack/stackusingvector.cpp
+++ Complete DSA In C++/14. Stack/stackusingvector.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
+#include "stackusingvector.h"
 using namespace std;
 
-int main ()
-{
+int main (){
+    
+    Stack <int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    while(!s.isEmpty()){
+        cout << s.peek() << " ";
+        s.pop();
+    }
+    cout << "\n";
     return 0;
 }
--- Complete DSA In C++/14. Stack/stackusingvector.h
+++ Complete DSA In C++/14. Stack/stackusingvector.h
@@ -1,2 +1,21 @@
 #include <vector>
 using namespace std;
+
+template<typename T>
+class Stack{
+   vector<T> arr;
+   public:
+   void push(int data){
+     arr.push_back(data);
+   }
+   bool isEmpty(){
+      return arr.size() == 0; 
+   }
+   T peek(){
+     int index = arr.size()-1;
+     return arr[index];
+   }
+   void pop(){
+     arr.pop_back();
+   }
+};
