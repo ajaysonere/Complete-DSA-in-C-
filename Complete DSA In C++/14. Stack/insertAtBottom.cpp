7f65693e901b4372ac9ea6176d1db99e42b9f3ca
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-02 23:23:30 +0530

    insert at bottom challange

--- Complete DSA In C++/14. Stack/insertAtBottom.cpp
+++ Complete DSA In C++/14. Stack/insertAtBottom.cpp
@@ -1,7 +1,39 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 
-int main ()
-{ 
+void insertAtBottom(stack<int> &s, int value){
+    if(s.empty()){
+        s.push(value);
+        return ;
+    }
+
+    int temp = s.top();
+    s.pop();
+
+    insertAtBottom(s,value);
+
+    s.push(temp);
+}
+
+int main (){ 
+    stack<int> s;
+    s.push(5);
+    s.push(4);
+    s.push(3);
+    s.push(2);
+
+    // while(!s.empty()){
+    //     cout << s.top() << "\n";
+    //     s.pop();
+    // }
+
+    insertAtBottom(s,1);
+    cout << "After inserting An element two the bottom " << "\n";
+    while (!s.empty())
+    {
+        cout << s.top() << "\n";
+        s.pop();
+    }
     return 0;
 }
