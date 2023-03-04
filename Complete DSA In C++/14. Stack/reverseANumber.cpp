149613d5b79aefe6918f261187727b541f2cb10d
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-04 23:59:22 +0530

    reverse number

--- Complete DSA In C++/14. Stack/reverseANumber.cpp
+++ Complete DSA In C++/14. Stack/reverseANumber.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
+#include<stack>
 using namespace std;
+
+void
+
+void reverseNumber(stack<int> &a){
+    if(a.empty() == true){
+        return ;
+    }
+    int temp = a.top();
+    a.pop();
+    reverseNumber(a);
+    insertAtBottom(temp , a);
+}
 
 int main ()
 {
+
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+
+    reverseNumber(s);
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+
     return 0;
 }
