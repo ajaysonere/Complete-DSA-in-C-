d047f2221b18bcdf9587d675bd6a525482c1c127
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-12 11:30:31 +0530

    stock span problem

--- Complete DSA In C++/14. Stack/StockSpanProblem.cpp
+++ Complete DSA In C++/14. Stack/StockSpanProblem.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+//Time complexity of This Naive Solution Will be O(n^2)
+void stockSpanProblem(vector<int> vec){
+    for(int i=0; i<vec.size(); i++){
+        int span =1;
+        for(int j= i-1; j>=0; j--){
+            if(vec[j] > vec[i]){
+                break;
+            }else{
+                span++;
+            }
+        }
+        cout << span << " ";
+    }
+}
 
 int main ()
 {
+    vector<int> vec(7);
+    for(int i= 0; i < 7; i++){
+        cin >> vec[i];
+    }
+    stockSpanProblem(vec);
     return 0;
 }
