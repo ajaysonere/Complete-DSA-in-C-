b15622ba49efe0b185e99595ec30bf49977a409b
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-12 18:08:06 +0530

    stocks

--- Complete DSA In C++/14. Stack/StockSpanProblem.cpp
+++ Complete DSA In C++/14. Stack/StockSpanProblem.cpp
@@ -1,28 +1,39 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
 
 //Time complexity of This Naive Solution Will be O(n^2)
-void stockSpanProblem(vector<int> vec){
-    for(int i=0; i<vec.size(); i++){
-        int span =1;
-        for(int j= i-1; j>=0; j--){
-            if(vec[j] > vec[i]){
-                break;
-            }else{
-                span++;
-            }
-        }
-        cout << span << " ";
-    }
-}
+// void stockSpanProblem(vector<int> vec){
+//     for(int i=0; i<vec.size(); i++){
+//         int span =1;
+//         for(int j= i-1; j>=0; j--){
+//             if(vec[j] > vec[i]){
+//                 break;
+//             }else{
+//                 span++;
+//             }
+//         }
+//         cout << span << " ";
+//     }
+// }
+
+//Time complexity of this Naive Solution Will be O(n) using stack
+// vector<int> stockSpanProblem(vector<int> &stockes){
+//     vector<int> ans;
+//     stack<int> s;
+    
+// }
 
 int main ()
 {
-    vector<int> vec(7);
+    vector<int> stockes(7);
     for(int i= 0; i < 7; i++){
-        cin >> vec[i];
+        cin >> stockes[i];
     }
-    stockSpanProblem(vec);
+    vector<int> ans  = stockSpanProblem(stockes);
+    for(int i = 0; i< ans.size() ; i++){
+        cout << ans[i] << " ";
+    }
     return 0;
 }
