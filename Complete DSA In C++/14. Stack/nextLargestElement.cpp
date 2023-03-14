29dceca4245c1b9340a28ad0cce118fdde4dbeb6
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-14 22:49:51 +0530

    next greatest element from right

--- Complete DSA In C++/14. Stack/nextLargestElement.cpp
+++ Complete DSA In C++/14. Stack/nextLargestElement.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
+#include<stack>
 using namespace std;
 
 
@@ -22,8 +23,29 @@ vector<int> nextGreatestElement(vector<int> vec)
     return ans;
 }
 
-vector<int> nextGreatestElement(vector<int> vec){
-    
+vector<int> nextGreatestElementOptimized(vector<int> vec){
+    stack<int> s;
+    vector<int> ans;
+    int size = vec.size()-1;
+    for(int i= size ; i>=0 ; i--){
+        if(s.empty()){
+            ans.push_back(-1);
+        }else if(s.top() > vec[i]){
+            ans.push_back(s.top());
+        }else{
+            while(!s.empty() && s.top() <= vec[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                ans.push_back(-1);
+            }else{
+                ans.push_back(s.top());
+            }
+        }
+        s.push(vec[i]);
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
 }
 
 int main()
@@ -36,7 +58,8 @@ int main()
     }
 
     // calling the next greatest element function
-    vector<int> ans = nextGreatestElement(vec);
+    // vector<int> ans = nextGreatestElement(vec);
+     vector<int> ans = nextGreatestElementOptimized(vec);
 
     // ans
     for (int i = 0; i < ans.size(); i++)
