324d204dd7d8b5954092d88ba80b495b766e8471
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-16 14:42:02 +0530

    stock span problem

--- Complete DSA In C++/14. Stack/StockSpanProblem.cpp
+++ Complete DSA In C++/14. Stack/StockSpanProblem.cpp
@@ -4,26 +4,50 @@
 using namespace std;
 
 //Time complexity of This Naive Solution Will be O(n^2)
-// void stockSpanProblem(vector<int> vec){
-//     for(int i=0; i<vec.size(); i++){
-//         int span =1;
-//         for(int j= i-1; j>=0; j--){
-//             if(vec[j] > vec[i]){
-//                 break;
-//             }else{
-//                 span++;
-//             }
-//         }
-//         cout << span << " ";
-//     }
-// }
+vector<int> stockSpanProblem(vector<int> vec){
+   vector<int> ans;
+   ans.push_back(1);
+   for(int i=1; i<vec.size(); i++){
+       int span = 1;
+       for(int j=i-1; j>=0; j--){
+          if(vec[j] > vec[i]){
+             ans.push_back(span);
+             break;
+          }else{
+            span++;
+          }
+       }
+   }
+   return ans;
+}
 
 //Time complexity of this Naive Solution Will be O(n) using stack
-// vector<int> stockSpanProblem(vector<int> &stockes){
-//     vector<int> ans;
-//     stack<int> s;
+vector<int> stockSpanProblemOptimized(vector<int> &stockes){
+    vector<int> ans;
+    stack<pair<int,int>> s;
+    for(int i=0; i<stockes.size(); i++){
+        if(s.empty()){
+            ans.push_back(-1);
+        }else if(s.top().first > stockes[i]){
+            ans.push_back(s.top().second);
+        }else{
+            while(!s.empty() && s.top().first <= stockes[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                ans.push_back(-1);
+            }else{
+                ans.push_back(s.top().second);
+            }
+        }
+        s.push({stockes[i],i});
+    }
+    for(int i=0; i<ans.size();i++){
+        ans[i] = i-ans[i];
+    }
+    return ans;
     
-// }
+}
 
 int main ()
 {
@@ -31,7 +55,8 @@ int main ()
     for(int i= 0; i < 7; i++){
         cin >> stockes[i];
     }
-    vector<int> ans  = stockSpanProblem(stockes);
+    vector<int> ans  = stockSpanProblemOptimized(stockes);
+
     for(int i = 0; i< ans.size() ; i++){
         cout << ans[i] << " ";
     }
