2f84afb8ddae1e87ccde1cdf45a05d1df1033c5f
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-19 15:33:01 +0530

    template

--- Complete DSA In C++/15. Queues/queueUsingArray.cpp
+++ Complete DSA In C++/15. Queues/queueUsingArray.cpp
@@ -1,7 +1,82 @@
 #include<iostream>
 using namespace std;
 
+class queue{
+    int *arr;
+    int length;
+    int front;
+    int rear;
+    int data;
+    public:
+    queue(int length){
+        this->length = length;
+        this->front = 0;
+        this->rear = -1;
+        arr = new int[length];
+    }
+    void push(int data);
+    void pop();
+    int queueFront();
+    int queueRear();
+    int size();
+    bool empty();
+};
+
+void queue::push(int data){
+   if(rear == length){
+      cout << "queue is already full  \n";
+   }else{
+      rear++;
+      arr[rear] = data;
+   }
+}
+
+void queue::pop(){
+    if(front == rear){
+        cout << "queue is empty \n";
+    }else{
+        front++;
+        if(front == rear){
+            front = 0;
+            rear =  0;
+        }
+    }
+}
+
+int queue::queueFront(){
+    if(front == rear){
+        cout << "Front is empty \n";
+    }else{
+        return arr[front];
+    }
+}
+
+int queue::queueRear(){
+    if(front == rear){
+        cout << "Rear is empty \n";
+    }else{
+        return arr[rear];
+    }
+}
+
+int queue::size(){
+    return length;
+}
+
+bool queue::empty(){
+    return (front == rear);
+}
+
+
 int main (){
-    // 
+    // Queue using the 
+    queue q(5);
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    cout << q.queueFront() << " " << q.queueRear() << "\n";
+    
     return 0;
 }
--- Complete DSA In C++/15. Queues/queueUsingLinkedList.cpp
+++ Complete DSA In C++/15. Queues/queueUsingLinkedList.cpp
@@ -1,30 +1,31 @@
 #include<iostream>
 using namespace std;
-
+template<typename T>
 class Node{
     public:
-    int data;
-    Node* next;
+    T data;
+    Node <T> * next;
 
-    Node(int data){
+    Node(T data){
         this->data = data;
         this->next = NULL;
     }
 };
-
-class queue{
-    Node* head ;
-    Node* tail ;
+template <typename T>
+class queue
+{
+    Node<T> * head ;
+    Node<T> * tail ;
     int length;
     public:
     queue(){
         this->head = NULL;
         this->tail = NULL;
-        this->length =0;
+        this->length = 0;
     }
 
-    void push(int data){
-        Node *temp = new Node(data);
+    void push(T data){
+        Node<T> *temp = new Node <T>(data);
         if(head == NULL){
             head = temp;
             tail = temp;
@@ -48,14 +49,14 @@ class queue{
         if(empty()){
             cout << "Queue is empty \n";
         }else{
-            Node* temp = head;
+            Node<T> * temp = head;
             head = head->next;
             length--;
             delete temp;
         }
     }
     
-    int front(){
+    T front(){
         if(head == NULL){
            return -1;
         }else{
@@ -63,7 +64,7 @@ class queue{
         }
     }
 
-    int back(){
+    T back(){
         if(tail == NULL){
             return -1;
         }else{
@@ -76,7 +77,7 @@ class queue{
             cout << "Queue has nothing to print \n";
             return;
         }
-        Node *temp = head;
+        Node<T>*temp = head;
         while(temp){
             cout << temp->data << " ";
             temp = temp->next;
@@ -87,31 +88,40 @@ class queue{
 
 int main ()
 {
-    queue q;
-    q.push(10);
-    q.push(20);
-    q.push(30);
-    q.push(40);
-    q.push(50);
-    q.print();
-    cout << "after pop first element "<< "\n";
-    q.pop();
-    q.print();
-    cout << "size of the " << q.size() << "\n";
-    cout << q.front() << " " << q.back() << "\n";
-    q.push(60);
-    cout << "after pop first element " << "\n";
-    q.pop();
-    q.push(71);
-    q.pop();
-    q.pop();
+    queue<string>q;
+    // q.push(10);
+    // q.push(20);
+    // q.push(30);
+    // q.push(40);
+    // q.push(50);
+    // q.print();
+    // cout << "after pop first element "<< "\n";
+    // q.pop();
+    // q.print();
+    // cout << "size of the " << q.size() << "\n";
+    // cout << q.front() << " " << q.back() << "\n";
+    // q.push(60);
+    // cout << "after pop first element " << "\n";
+    // q.pop();
+    // q.push(71);
     // q.pop();
     // q.pop();
-    // q.pop();
+    // // q.pop();
+    // // q.pop();
+    // // q.pop();
+    // q.print();
+    // cout << "size of the " << q.size() << "\n";
+    // cout << q.front() << " " << q.back() << "\n";
+    // q.empty()?cout << "Queue is empty \n":cout<< "Queue is not Empty \n";
+
+    q.push("ajay");
+    q.push("vijay");
+    q.push("anjali");
     q.print();
-    cout << "size of the " << q.size() << "\n";
-    cout << q.front() << " " << q.back() << "\n";
-    q.empty()?cout << "Queue is empty \n":cout<< "Queue is not Empty \n";
+    q.pop();
+    q.print();
+    cout << "size is :" << q.size() << "\n";
+
 
     return 0;
 }
