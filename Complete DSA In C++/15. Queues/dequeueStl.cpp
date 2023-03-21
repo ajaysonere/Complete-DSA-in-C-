0db185dd880d68e413ea057f2d3f90dd24f1832d
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-21 07:34:18 +0530

    dequeue using stl

--- Complete DSA In C++/15. Queues/dequeueStl.cpp
+++ Complete DSA In C++/15. Queues/dequeueStl.cpp
@@ -1,7 +1,18 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-int main ()
+int main()
 {
+    deque<int> d;
+    d.push_front(18);
+    cout << d.front() << "\n";
+    d.push_back(20);
+    cout << d.back() << "\n";
+    d.pop_back();
+    cout << d.front() << "\n";
+    cout << d.back() << "\n";
+    d.pop_back();
+    d.empty() ? cout << "Dequeue is empty " : cout << "Dequeue is not empty";
     return 0;
 }
