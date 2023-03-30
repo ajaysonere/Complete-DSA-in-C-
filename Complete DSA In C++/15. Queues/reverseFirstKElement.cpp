ee495dbb32895a03172d66442ff1394493b32d2e
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-30 15:55:25 +0530

    reverse first k element in queue

--- Complete DSA In C++/15. Queues/reverseFirstKElement.cpp
+++ Complete DSA In C++/15. Queues/reverseFirstKElement.cpp
@@ -1,7 +1,48 @@
 #include<iostream>
+#include<queue>
+#include<stack>
 using namespace std;
+
+void reverseQueueInGroup(deque<int> &d, int k)
+{
+    stack<int> s;
+    int count = 0 ;
+    while(count < k){
+        int element = d.front();
+        d.pop_front();
+        s.push(element);
+        count++;
+    }
+    while(!s.empty()){
+        int element = s.top();
+        s.pop();
+        d.push_back(element);
+    }
+
+    count = 0;
+    while(count < d.size()-k){
+        int element = d.front();
+        d.pop_front();
+        d.push_back(element);
+        count++;
+    }
+}
 
 int main ()
 {
+    deque<int> d;
+    d.push_back(1);
+    d.push_back(2);
+    d.push_back(3);
+    d.push_back(4);
+    d.push_back(5);
+
+    int k;
+    cin >> k;
+    reverseQueueInGroup(d,k);
+    while(!d.empty()){
+        cout << d.front() << " ";
+        d.pop_front();
+    }
     return 0;
 }
