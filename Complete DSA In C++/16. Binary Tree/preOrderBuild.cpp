da11189b41076482999709fc7c68f6ac239a75fd
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-02 08:33:55 +0530

    preOrder build

--- Complete DSA In C++/16. Binary Tree/preOrderBuild.cpp
+++ Complete DSA In C++/16. Binary Tree/preOrderBuild.cpp
@@ -1,7 +1,46 @@
 #include<iostream>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildTree(){
+    cout <<"Enter data : \n";
+    int data;
+    cin >> data;
+
+    if(data == -1){
+        return NULL;
+    }
+    Node* root = new Node(data);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void preOrder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+
 int main ()
 {
+    Node* root = buildTree();
+    preOrder(root);
     return 0;
 }
