2b531f201adf4ed6e79493ae15f47de482e8ef32
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-02 14:48:31 +0530

    count the leaf nodes

--- Complete DSA In C++/16. Binary Tree/countLeafNodesOfTree.cpp
+++ Complete DSA In C++/16. Binary Tree/countLeafNodesOfTree.cpp
@@ -1,7 +1,51 @@
 #include<iostream>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildTree(Node* root){
+    cout << "Enter data :" << "\n";
+    int data;
+    cin >> data;
+    root = new Node(data);
+
+    if(data == -1){
+        return NULL;
+    }
+    cout << "Enter the data for left " << data << "\n";
+    root->left = buildTree(root->left);
+    cout << "Enter the data for right " << data << "\n";
+    root->right = buildTree(root->right);
+    return root;
+}
+
+void countLeafNode(Node* root , int &count){
+    if(root == NULL){
+        return ;
+    }
+    countLeafNode(root->left,count);
+    if(root->left == NULL && root->right == NULL){
+        count++;
+    }
+    countLeafNode(root->right ,count);
+}
+
 int main ()
 {
+    Node* root = buildTree(root);
+    int count = 0;
+    countLeafNode(root,count);
+    cout << "Number of Leaf Nodes " << count << "\n";
     return 0;
 }
