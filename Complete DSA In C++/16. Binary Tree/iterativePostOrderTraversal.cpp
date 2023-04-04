01698d146ec8925999955dcea0ff72013086c5a5
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-04 17:21:10 +0530

    post order traversal using iterative

--- Complete DSA In C++/16. Binary Tree/iterativePostOrderTraversal.cpp
+++ Complete DSA In C++/16. Binary Tree/iterativePostOrderTraversal.cpp
@@ -1,7 +1,76 @@
 #include<iostream>
+#include<stack>
+#include<vector>
 using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildTree(){
+    int data;
+    cin >> data;
+
+    Node* root = new Node(data);
+
+    if(data == -1){
+        return NULL;
+    }
+
+    cout << "Enter the data for Left Node " << data << "\n";
+    root->left = buildTree();
+    cout << "Entre the data for right Node " << data << "\n";
+    root->right = buildTree();
+    return root;
+}
+
+void printPostOrder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+    printPostOrder(root->left);
+    printPostOrder(root->right);
+    cout << root->data << " ";
+}
+
+void printPostOrderIterative(Node* root){
+    if(root == NULL){
+        return;
+    }
+    stack<Node* > s1,s2;
+    s1.push(root);
+    while(!s1.empty()){
+        root = s1.top();
+        s1.pop();
+        s2.push(root);
+        if(root->left){
+            s1.push(root->left);
+        }
+        if(root->right){
+            s1.push(root->right);
+        }
+    }
+    while(!s2.empty()){
+        cerr << s2.top()->data << " ";
+        s2.pop();
+    }
+}
 
 int main ()
 {
+    Node* root = buildTree();
+    cout << "Post Order Traversal Using The Recursion " << "\n";
+    printPostOrder(root);
+    cout << "\nPost Order Traversal Using The Iteration " << "\n";
+    printPostOrderIterative(root);
     return 0;
 }
