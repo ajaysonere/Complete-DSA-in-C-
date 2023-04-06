e58f87b395bdaa49dc518023ee340043dcd6a614
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-06 17:11:48 +0530

    expression traversal

--- Complete DSA In C++/16. Binary Tree/expressionTree.cpp
+++ Complete DSA In C++/16. Binary Tree/expressionTree.cpp
@@ -1,7 +1,63 @@
 #include<iostream>
+#include<stack>
 using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* BuildTree(){
+    int data;
+    cin >> data;
+
+    Node* root = new Node(data);
+    if(data == -1){
+        return NULL;
+    }
+    root->left = BuildTree();
+    root->right = BuildTree();
+    return root;
+}
+
+void printPostOrder(Node* root){
+    stack<Node* > s;
+    Node* curr = root;
+    while(curr != NULL || !s.empty()){
+        if(curr != NULL){
+            s.push(curr);
+            curr = curr->left;
+        }else{
+            root = s.top()->right;
+            if(root == NULL){
+                root = s.top();
+                s.pop();
+                cout << root->data << " ";
+                while(!s.empty() && root == s.top()->right){
+                    root = s.top();
+                    s.pop();
+                    cout << root->data << " ";
+                }
+            }else{
+                curr = root;
+            }
+        }
+    }
+}
 
 int main ()
 {
+    Node* root = BuildTree();
+    cout <<"Post Order " << "\n";
+    printPostOrder(root);
+    cout << "\n";
     return 0;
 }
