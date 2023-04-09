1921668e35d73a1627ea833f1361f1e5aa0894bb
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-09 18:37:29 +0530

    ordered set in c++

--- Complete DSA In C++/19. Hashing/set.cpp
+++ Complete DSA In C++/19. Hashing/set.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
+#include<set>
 using namespace std;
 
-int main ()
-{
+int main (){
  
+    set<int> s;
+    s.insert(10);
+    s.insert(20);
+    s.insert(30);
+    s.insert(40);
+    s.insert(40);
+
+    for(set<int>::iterator itr = s.begin() ; itr != s.end(); itr++){
+        cout << *itr << " ";
+    }
+    cout <<"\n";
+    s.erase(30);
+    for (set<int>::iterator itr = s.begin(); itr != s.end(); itr++)
+    {
+        cout << *itr << " ";
+    }
+    cout <<"\n";
+    if(s.find(20) == s.end()){
+        cout << "Element is not present " << "\n";
+    }else{
+        cout << "Element is present " << "\n";
+    }
+
+    cout << "Size of the set " << s.size() << "\n";
     return 0;
 }
