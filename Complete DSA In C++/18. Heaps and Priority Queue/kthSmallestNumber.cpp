f835fb6598b4ad8db5c907c0ab7a9a33912da108
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-13 19:41:06 +0530

    k th smallest element

--- Complete DSA In C++/18. Heaps and Priority Queue/kthSmallestNumber.cpp
+++ Complete DSA In C++/18. Heaps and Priority Queue/kthSmallestNumber.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
-#include<queue>
+#include<algorithm>
 using namespace std;
+
+
+// naive approach
+int kthSmallest(int arr[], int size,int k){
+    sort(arr,arr+size);
+    k--;
+    return arr[k];
+}
 
 int main ()
 {
-    
+    int size;
+    cin >> size;
+    int arr[size];
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+    int ans = kthSmallest(arr,size,k);
+    cout << "Kth Smallest " << ans << "\n";
     return 0;
 }
