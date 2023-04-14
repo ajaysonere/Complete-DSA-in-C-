75e76a3b425f89f93dccd2ad3214c605981af5d9
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-14 16:08:15 +0530

    reduced array size to half

--- Complete DSA In C++/18. Heaps and Priority Queue/reduceArraySizeToHalf.cpp
+++ Complete DSA In C++/18. Heaps and Priority Queue/reduceArraySizeToHalf.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 #include<vector>
 #include<map>
+
 using namespace std;
+
+int reduceArraySizeToHalf(vector<int>&arr){
+    map<int,int>mp;
+    for(auto key : arr){
+        mp[key]++;
+    }
+    multimap<int , int , greater<int>> mpp;
+    for(auto x : mp){
+        mpp.insert({x.second , x.first});
+    }
+    // for(auto it = mpp.begin(); it != mpp.end(); it++){
+    //     cout << it->first << " " << it->second << "\n";
+    // }
+    int size = arr.size();
+    int sum = size;
+    int count = 0;
+    for(auto it = mpp.begin(); it != mpp.end(); it++){
+        sum = sum - it->first;
+        count++;
+        if(sum <= size/2){
+          break;
+        }
+    }
+    return count;
+}
 
 int main ()
 {
