34e8a9a68524d6fb587d79cc48084244f8d6466d
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-16 15:09:04 +0530

    k largest element

--- Complete DSA In C++/18. Heaps and Priority Queue/klargestElement.cpp
+++ Complete DSA In C++/18. Heaps and Priority Queue/klargestElement.cpp
@@ -3,11 +3,43 @@
 #include<queue>
 using namespace std;
 
+vector<int> kLargestElement(vector<int>vec , int k){
+    vector<int> ans;
+    priority_queue<int, vector<int> , greater<int>> pq;
+    // inserting element in min heap 
+    for(int i=0; i<vec.size(); i++){
+        pq.push(vec[i]);
+        if(pq.size()>k){
+            pq.pop();
+        }
+    }
+
+    // storing the k largest element in vector
+    while(!pq.empty()){
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    // return ans
+    return ans;
+}
+
 int main ()
 {
     int size;
     cin >> size;
     vector<int> vec(size);
-    
+    // taking the element 
+    for(int i=0; i<vec.size(); i++){
+        cin >> vec[i];
+    }
+
+    int k;
+    cin >> k;
+    vector<int> ans = kLargestElement(vec,k);
+    cout <<"K Largest Element is " << "\n";
+    for(int i=0; i<ans.size(); i++){
+      cout << ans[i] << " ";
+    }
+
     return 0;
 }
