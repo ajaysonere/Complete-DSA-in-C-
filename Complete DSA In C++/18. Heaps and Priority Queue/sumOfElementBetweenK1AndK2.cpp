0d4e9ac6abf5ced099cd583081bfddd52a5caf09
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-19 18:28:27 +0530

    sum of element between k smallest number

--- Complete DSA In C++/18. Heaps and Priority Queue/sumOfElementBetweenK1AndK2.cpp
+++ Complete DSA In C++/18. Heaps and Priority Queue/sumOfElementBetweenK1AndK2.cpp
@@ -3,6 +3,29 @@
 #include<queue>
 using namespace std;
 
+int smallestElement(vector<int> arr , int k1){
+    priority_queue<int> p;
+    for(int i=0; i<arr.size(); i++){
+        p.push(arr[i]);
+        if(p.size()>k1){
+            p.pop();
+        }
+    }
+    return p.top();
+}
+
+int sumOfSmallestElementBetweenK1AndK2(vector<int> arr, int k1, int k2){
+    int kThSmallestElement = smallestElement(arr,k1);
+    int kThSmallestElement2 = smallestElement(arr, k2);
+    int sum =0;
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i] > kThSmallestElement && arr[i] < kThSmallestElement2){
+          sum +=arr[i];
+        }
+    }
+    return sum;
+}
+
 int main ()
 {
     int size;
@@ -11,6 +34,9 @@ int main ()
     for(int i=0; i<arr.size(); i++){
         cin >> arr[i];
     }
-    
+    int k1,k2;
+    cin >> k1 >> k2;
+    int ans = sumOfSmallestElementBetweenK1AndK2(arr,k1,k2);
+    cout << "Sum of the Element is " << ans << "\n";
     return 0;
 }
