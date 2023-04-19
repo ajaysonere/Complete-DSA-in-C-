87d14d3dc3cdf3775a8b6d566ba46b1718b82881
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-19 18:16:06 +0530

    sum of element between k1 and k2

---
+++ Complete DSA In C++/18. Heaps and Priority Queue/sumOfElementBetweenK1AndK2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main ()
+{
+    int size;
+    cin >> size;
+    vector<int> arr(size);
+    for(int i=0; i<arr.size(); i++){
+        cin >> arr[i];
+    }
+    
+    return 0;
+}
