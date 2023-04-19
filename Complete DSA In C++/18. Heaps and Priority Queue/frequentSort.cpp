ce254d4081c15a9b0a5e3d541aecd431b9053111
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-19 16:31:55 +0530

    frequent sort

--- Complete DSA In C++/18. Heaps and Priority Queue/frequentSort.cpp
+++ Complete DSA In C++/18. Heaps and Priority Queue/frequentSort.cpp
@@ -6,7 +6,29 @@
 using namespace std;
 
 vector<int> frequentSort(vector<int> arr ){
-    
+    unordered_map<int,int> um;
+
+    for(int i=0; i<arr.size(); i++){
+        um[arr[i]]++;
+    }
+
+    priority_queue<pair<int,int>> p;
+
+    for(auto i= um.begin(); i!= um.end(); i++){
+       p.push({i->second , i->first});
+    }
+
+    vector<int> ans;
+    while(!p.empty()){
+        int freq = p.top().first;
+        int element = p.top().second;
+        while(freq--){
+            ans.push_back(element);
+        }
+        p.pop();
+    }
+    return ans;
+
 }
 
 int main ()
