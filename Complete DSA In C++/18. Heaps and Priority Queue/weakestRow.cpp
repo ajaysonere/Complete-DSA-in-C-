5d2f6cbfdbe9b702af6e0e29384980260916694b
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-20 22:32:42 +0530

    vector

--- Complete DSA In C++/18. Heaps and Priority Queue/weakestRow.cpp
+++ Complete DSA In C++/18. Heaps and Priority Queue/weakestRow.cpp
@@ -11,10 +11,20 @@ int main ()
     for(int i=0; i<row;i++){
         vector<int> temp;
         for(int i=0; i<col; i++){
-           cin >> temp[i];
+           int value;
+           cin >> value;
+           temp.push_back(value);
         }
         arr.push_back(temp);
     }
-    
+
+    // printing the vector
+    cout << "Our Vector is " << "\n";
+    for(int i=0; i<arr.size(); i++){
+        for(int j=0; j<arr[i].size(); j++){
+            cout << arr[i][j] << " ";
+        }
+        cout <<"\n";
+    }
     return 0;
 }
