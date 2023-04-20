25ce010457b0c52959c81238e5637cbd24d1ce25
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-20 22:18:57 +0530

    2d vector

--- Complete DSA In C++/18. Heaps and Priority Queue/weakestRow.cpp
+++ Complete DSA In C++/18. Heaps and Priority Queue/weakestRow.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main ()
 {
+    int row,col;
+    cin >> row >> col;
+    vector<vector<int>>arr;
+
+    for(int i=0; i<row;i++){
+        vector<int> temp;
+        for(int i=0; i<col; i++){
+           cin >> temp[i];
+        }
+        arr.push_back(temp);
+    }
+    
     return 0;
 }
