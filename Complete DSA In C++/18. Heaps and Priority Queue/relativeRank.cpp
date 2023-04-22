eb818d0d6dd14560e1e50a066efa7dcac35cbc74
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-22 13:03:47 +0530

    relative time

--- Complete DSA In C++/18. Heaps and Priority Queue/relativeRank.cpp
+++ Complete DSA In C++/18. Heaps and Priority Queue/relativeRank.cpp
@@ -1,7 +1,48 @@
 #include<iostream>
+#include<vector>
+#include<queue>
+#include<string>
+#include<algorithm>
 using namespace std;
+
+vector<string> relativeRanks(vector<int>arr){
+    priority_queue<pair<int,int>> p;
+
+    for(int i=0;i<arr.size();i++){
+        p.push({arr[i],i+1});
+    }
+    vector<string> ans(arr.size());
+
+    ans[p.top().second-1] = "Gold Medal";
+    p.pop();
+    ans[p.top().second-1] = "Silver Medal";
+    p.pop();
+    ans[p.top().second-1] = "Bronze Medal";
+    p.pop();
+    int k=3;
+    while(!p.empty()){
+        k++;
+        string ansH = to_string(k);
+        ans[p.top().second-1] = ansH;
+        p.pop();
+    }
+    return ans;
+
+}
 
 int main ()
 {
+    int size;
+    cin >> size;
+    vector<int> arr(size);
+
+    for(int i=0; i<size;i++){
+        cin >> arr[i];
+    }
+    
+    vector<string>ans = relativeRanks(arr);
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
     return 0;
 }
