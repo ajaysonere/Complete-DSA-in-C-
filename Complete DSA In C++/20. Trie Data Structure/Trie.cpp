91f2efa84498546d6b296ea8728919e0baec011d
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-23 10:21:59 +0530

    trie creation

--- Complete DSA In C++/20. Trie Data Structure/Trie.cpp
+++ Complete DSA In C++/20. Trie Data Structure/Trie.cpp
@@ -1,6 +1,43 @@
 #include<iostream>
 using namespace std;
 
+class TrieNode{
+    public:
+      char data;
+      TrieNode* childNode[26];
+      bool isTerminal;
+
+    TrieNode(char data){
+        this->data = data;
+        for(int i=0; i<26;i++){
+            childNode[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie{
+    public:
+
+    TrieNode* root;
+
+    void insertUtill(TrieNode* root , string str){
+        // base condition
+        if(str.length() == 0){
+            root->isTerminal = true;
+            return ;
+        }
+
+        // assumption , string will be in the Caps
+        
+    }
+
+    void insertWord(string str){
+        insertUtill(root , str);
+    }
+
+}
+
 int main ()
 {
     return 0;
