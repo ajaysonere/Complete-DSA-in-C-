d9d3c37f2f9f78abe4df63f6667abbce772fc3f0
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-24 14:35:10 +0530

    trie

--- Complete DSA In C++/20. Trie Data Structure/Trie.h
+++ Complete DSA In C++/20. Trie Data Structure/Trie.h
@@ -1,3 +1,50 @@
+#include<unordered_map>
+#include<string>
+using namespace std;
 class Node{
-    
-}
+    char data;
+    unordered_map<char , Node*>children ;
+    bool isTerminal;
+
+    public:
+    Node(char data){
+        this->data = data;
+        isTerminal = false;
+    }
+    friend class Trie;
+};
+
+class Trie{
+    Node* root;
+    public:
+    Trie(){
+        root = new Node('\0');
+    }
+    // insertion in trie 
+
+    void insertWord(string word){
+        Node* temp = root;
+
+        for(auto ch : word){
+            if(temp->children.count(ch) == 0){
+                Node* n = new Node(ch);
+                temp->children[ch] = n;
+            }
+            temp = temp->children[ch];
+        }
+        temp->isTerminal = true;
+    }
+
+    // searching in trie
+    bool searchWord(string word){
+        Node* temp = root;
+        for(auto ch : word){
+            if(temp->children.count(ch) == 0){
+                return false;
+            }
+            temp = temp->children[ch];
+        }
+        return temp->isTerminal;
+    }
+
+};
