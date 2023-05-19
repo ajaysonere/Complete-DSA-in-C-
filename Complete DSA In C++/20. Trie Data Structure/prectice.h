ac2deb85f347624893587f8161cdceab27d44171
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-19 16:54:39 +0530

    prectice

--- Complete DSA In C++/20. Trie Data Structure/prectice.h
+++ Complete DSA In C++/20. Trie Data Structure/prectice.h
@@ -1,6 +1,48 @@
 #include<unordered_map>
+#include<string>
 
 class Node{
+    char data;
+    unordered_map<char , Node*> children;
+    bool isTerminal;
     public:
-    
-}
+    Node(char data){
+        this->data = data;
+        this->isTerminal = false;
+    }
+    friend class Trie;
+};
+
+class Trie{
+    Node* root;
+    public:
+
+    Trie(){
+        root = new Node('\0');
+    }
+
+    // for inserting the 
+    void insert(string str){
+        Node* temp = root;
+        for(char ch : str){
+            if(temp->children.count(ch) == 0){
+                Node* n = new Node(ch);
+                temp->children[ch] = n;
+            }
+        }
+        temp->isTerminal = true;
+    }
+
+    //for searching the
+    void search(string str){
+        Node* temp = root;
+        for(char ch : str){
+            if(temp->children.count(ch)==0){
+                return false;
+            }
+            temp = temp->children[ch];
+        }
+        return temp->isTerminal;
+    }
+
+};
