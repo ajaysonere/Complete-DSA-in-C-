0216a4af1b1bb3260e6c64448b7b07f98d66c23e
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-21 09:04:26 +0530

    find all subsets

--- Complete DSA In C++/10. Backtracking/findAllSubsets.cpp
+++ Complete DSA In C++/10. Backtracking/findAllSubsets.cpp
@@ -3,15 +3,28 @@ using namespace std;
 
 void findAllSubsets(char input[], char output[], int i, int j){
     // base case
-    if()
+    if(input[i] == '\0'){
+        output[j] = '\0';
+        cout << output << endl;
+        return ;
+    }
 
     // recursive case
+    // include the character
+
+    output[j] = input[i];
+    findAllSubsets(input , output, i+1, j+1);
+
+    // exclude the character 
+    findAllSubsets(input , output , i+1, j);
+
 }
 
 int main(){
     char input[100];
     char output[100];
     cin >> input;
+    cout << "Subsets are :) " << "\n";
     findAllSubsets(input , output , 0, 0);
     return 0;
 }
