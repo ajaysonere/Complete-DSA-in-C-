eb72dde6c2ffd33f74ce6c4ee0473f887c0686f2
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-21 09:26:43 +0530

    find all subsets

--- Complete DSA In C++/10. Backtracking/findAllSubsets.cpp
+++ Complete DSA In C++/10. Backtracking/findAllSubsets.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
-void findAllSubsets(char input[], char output[], int i, int j){
+bool compare(string a , string b){
+    if(a.length() == b.length()){
+        return a < b;
+    }
+    return a.length() < b.length();
+}
+
+void findAllSubsets(char input[], char output[], int i, int j , vector<string> &list){
     // base case
     if(input[i] == '\0'){
         output[j] = '\0';
-        cout << output << endl;
+        string temp = output;
+        list.push_back(temp);
         return ;
     }
 
@@ -13,10 +23,10 @@ void findAllSubsets(char input[], char output[], int i, int j){
     // include the character
 
     output[j] = input[i];
-    findAllSubsets(input , output, i+1, j+1);
+    findAllSubsets(input , output, i+1, j+1 , list);
 
     // exclude the character 
-    findAllSubsets(input , output , i+1, j);
+    findAllSubsets(input , output , i+1, j , list);
 
 }
 
@@ -25,6 +35,12 @@ int main(){
     char output[100];
     cin >> input;
     cout << "Subsets are :) " << "\n";
-    findAllSubsets(input , output , 0, 0);
+    vector<string> list;
+    findAllSubsets(input , output , 0, 0 , list);
+    sort(list.begin(),list.end(),compare);
+
+    for(auto x : list){
+        cout << x << " ";
+    }
     return 0;
 }
