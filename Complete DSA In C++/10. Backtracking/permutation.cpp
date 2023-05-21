9e38024aeca79fc927ad587a22b2c4a7753120b8
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-21 10:13:38 +0530

    permutation

--- Complete DSA In C++/10. Backtracking/permutation.cpp
+++ Complete DSA In C++/10. Backtracking/permutation.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
 
+void findPermutation(string str, int pos,  vector<string> &ans){
+    // base condition
+    if(pos >= str.size()){
+       ans.push_back(str);
+       return ;
+    }
+
+    // recursion
+    for(int i = pos; i<str.size(); i++){
+        swap(str[i], str[pos]);
+        findPermutation(str , pos+1, ans);
+        swap(str[i] , str[pos]);
+    }
+}
+
 int main(){
-    
+    string str;
+    cin >> str;
+    vector<string> ans ;
+    findPermutation(str , 0 , ans);
+    for(auto x : ans){
+        cout << x << " ";
+    }
     return 0;
 }
